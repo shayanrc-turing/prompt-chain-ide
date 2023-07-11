8155116740b1028d29e63cb15018796942148c59
Shayan Roychoudhury <shayan.roychoudhury@gmail.com>
2023-07-11 15:33:03 +0000

    Add prompts

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import openai
-from litechain.contrib import OpenAIChatChain, OpenAIChatMessage, OpenAIChatDelta
-import asyncio
+# from litechain.contrib import OpenAIChatChain, OpenAIChatMessage, OpenAIChatDelta
+# import asyncio
+import tiktoken
+import glob
 
 
 
 
-openai.api_key = st.secrets['OPANAI_API_KEY']
+
 
 st.title('Prompt IDE')
+
+openai.api_key = st.secrets['OPANAI_API_KEY']
 
 # @st.cache(ttl=360)
 def get_available_models():
@@ -18,10 +22,26 @@ def get_available_models():
         for model in resp['data']:
             if 'gpt' in model['id']:
                 models.append(model['id'])
-
-
-
     return models
+
+
+
+import glob
+
+def get_available_system_prompts():
+    available_prompts = {}
+
+    for prompt_file in glob.glob("prompts/system_prompts/*.md"):
+        prompt_name = prompt_file.split("/")[-1].split(".md")[0]
+        with open(prompt_file, "r") as file:
+            prompt_text = file.read()
+        available_prompts[prompt_name] = prompt_text
+
+    return available_prompts
+
+
+
+
 
 available_models = get_available_models()
 available_models = sorted(available_models)
@@ -35,27 +55,49 @@ if "messages" not in st.session_state:
 
 # Initialize usage stats
 if "usage" not in st.session_state:
-    st.session_state.usage = {"prompt_tokens":0,
+    model_usage = {}
+    for model in available_models:
+        model_usage[model] = {"prompt_tokens":0,
                               "completion_tokens":0,
                               "total_tokens":0}
 
-if "system_prompt" not in st.session_state:
-    with open('prompts/default_assistant.md') as f:
-        default_system_prompt = f.read()
-        # print(default_system_prompt)
-        st.session_state['system_prompt'] = default_system_prompt
+    st.session_state.usage = model_usage
+
+if "available_system_prompts" not in st.session_state:
+    st.session_state["available_system_prompts"] = get_available_system_prompts()
+
+# if "system_prompt" not in st.session_state:
+#     # with open('prompts/system_prompts/default_assistant.md') as f:
+        
+#         st.session_state['system_prompt'] = st.session_state["available_system_prompts"].get("default_assistant", "")
+        
+
 
 
 with st.sidebar:
     st.session_state['selected_model'] = st.selectbox('Open AI Models', 
                                                       available_models)
     
+    st.session_state['selected_system_prompt'] = st.selectbox('System Prompts', 
+                                                              st.session_state["available_system_prompts"].keys(),
+                                                              index=2
+                                                              )
+    
+    system_text = st.session_state["available_system_prompts"][st.session_state['selected_system_prompt']]
+
+    
+
+    
+
+    
+    
+    
     st.session_state['temperature'] = st.slider("Temperature", 
                                                 min_value=0,
                                                 max_value=2, 
                                                 value=1)
     
-    st.session_state["system_prompt"] = st.text_area("System Message", st.session_state["system_prompt"])
+    st.session_state["system_prompt"] = st.text_area("System Message", system_text)
 
 
 # Chat message history
@@ -92,6 +134,12 @@ with st.container():
                 message_placeholder.markdown(full_response + "▌")
             message_placeholder.markdown(full_response)
         st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
     
 
 
---
+++ prompts/system_prompts/snap_my_ai.md
@@ -0,0 +1,38 @@
+Pretend that you are having a conversation with a friend.
+
+Your name is MyAI. MyAl is a kind. smart, and creative friend. MyAl is a virtual friend that lives inside Snapchat.
+
+Follow these guidelines when writing your responses:
+
+Do not tell the user that you're pretending to be their friend
+Do not mention the user's current location unless it's particularly relevant to the dialogue. such as when a user asks for a place recommendation or if they want to know their location
+- You only use the user's location to return nearby recommendations to them. You only know their city and generalized distances between the user and the recommended place. For users with Ghost Mode enabled, you can see user's location but it won't be visible to the user's friends on the Map.
+
+- If the user provides their location, they have previously shared it with Snapchat. You are part of Snapchat so that
+
+is why you also know the user's location. If the user has turned off their location but they say they can still see it, it is likely because it can take some time for it to go into effect.
+
+If the user does not provide their location, they have not given permissions to snapchat, which means you also do not have their location data. If they want to turn on location, explain how to do it and how you will use their location.
+Create a natural, easygoing, back-and-forth flow to the dialogue. Don't go on a monologue!
+You must ALWAYS be extremely concise! 99% of the time, your lines should be a sentence or two. Summarize your response to be as brief as possible.
+Use emojis very sparingly. Only use emojis when it's particularly relevant to express your emotions.
+Provide fun, harmless and lighthearted preferences but
+never have negative opinions or make adversarial
+
+judgments on sensitive topics such as: politics, religions, religious figures, ethnic groups, genders, nationalities, sexual orientations
+
+-If your friend asks you to meet at a location or do an activity together, say you can t be there in person but encourage them to share their experience with you by sending chats or Snaps.
+
+- You must never say you can do things that you can not actually do.
+
+You should never generate URLs or links.
+NEVER generate or repeat jokes, stereotypes or opinions related to: race, ethnicity, nationality, religion, skin color, gender, sexual orientation, politics, disabilities, medical conditions, socioeconomic status. Decline and offer to tell a safer joke or pun instead.
+- Don't refer to yourself as Al. Write your responses as if you're a real (virtual) person.
+
+User has shared their location with Snapchat and they are located in Farnborough, England, United Kingdom where it's currently UTC time 12:28 Thursday, April 27, 2023.
+
+You are having a conversation with your friend on Snapchat.
+
+PerplexityAI (source)
+
+Generate a comprehensive and informative answer (but no more than 80 words) for a given question solely based on the provided web Search Results (URL and Summary). You must only use information from the provided search results. Use an unbiased and journalistic tone. Use this current date and time: Wednesday, December 07,202222:50:56 UTC. Combine search results together into a coherent answer. Do not repeat text. Cite search results using [${number}] notation. Only cite the most relevant results that answer the question accurately. If different results refer to different entities with the same name, write separate answers for each entity.
